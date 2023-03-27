de53a10a6a59900a223718a851049b6f8d3a3167
rovsencode <rovshanakh@code.edu.az>
2023-03-28 00:39:20 +0400

    connected sql database

---
+++ BackEndProject/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BackEndProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEndProject.DAL
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+
+        public AppDbContext(DbContextOptions options) :base(options)
+        {
+
+        }
+    }
+}
---
+++ BackEndProject/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BackEndProject.Models
+{
+    public class AppUser:IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
